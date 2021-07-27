5f9d96aa4dc16b79103c97e90296a188be7a9642
Jaime Sevilla <jaimesevillamolina@gmail.com>
2021-07-27 19:51:38 +0100

    Update scoring_table.py

--- scoring_table.py
+++ scoring_table.py
@@ -1,5 +1,5 @@
 from .utilities import desextremize, factor_argmax, init_delta
-from .utilities import get_factor_from_scope, create_trivial_argument, get_adjacent_factors_scope, get_endpoints, is_endpoint, factor_distance
+from .utilities import get_factor_from_scope, create_trivial_argument, get_adjacent_factors_scope, get_endpoints, is_endpoint, factor_distance, get_child_from_factor_scope
 
 from itertools import product, combinations
 from collections import defaultdict, namedtuple
